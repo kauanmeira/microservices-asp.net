ba2703ce01e2c6db9759f94fd859473e8679e64d
kauan.meira <kauangabriel.meira123@gmail.com>
2024-03-11 13:54:39 -0300

    Criação database e Repository

---
+++ GeekShopping.ProductAPI/Model/Context/SqlContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(){}
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,11 +1,18 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEntityFrameworkSqlServer()
+               .AddDbContext<SqlContext>(
+                   options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
+               );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
